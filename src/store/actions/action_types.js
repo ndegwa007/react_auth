c8afdfe79e2b7d23c68a376290a48c41aee2cf60
ndegz <ndegwa8781@gmail.com>
2023-10-18 10:27:12 +0300

    added some action reducers that will update state

---
+++ src/store/actions/action_types.js
@@ -0,0 +1,17 @@
+// hold all the string actions types in variables
+
+// boiler plate action
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+// update authentication state of user
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+// save profile data from AuthO to the global state
+export const ADD_PROFILE = 'ADD_PROFILE';
+export const REMOVE_PROFILE = 'REMOVE_PROFILE';
+
+// track changes and submit of the user submitted text of the form
+export const USER_INPUT_CHANGE = 'USER_INPUT_CHANGE';
+export const USER_INPUT_SUBMIT = 'USER_INPUT_SUBMIT';
